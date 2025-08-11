4e00334e37026af9a10a18fa3ad1c0680bf8d42a
rt-rocket <108962288+rt-rocket@users.noreply.github.com>
2025-08-10 21:05:21 -0700

    update assets

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -191,7 +191,7 @@ const Index = () => {
               Arcaea Charts
             </h1>
             <img
-              src="/arcaea_logo.jpg"
+              src="/logo.jpg"
               alt="Game Logo"
               className="h-16 md:h-20 w-auto object-contain"
             />
